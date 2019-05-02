76adc8d3202fa8c4919aac873650f597f5696b2b
Garvati <43912008+Garvati@users.noreply.github.com>
2019-05-02 14:01:13 -0500

    Add files via upload
    Created the login to function correctly, added php.ini.

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
-<?
-//include('login.html');
+<?php
+include('php.ini');
 $servername = "localhost";
 $username = "root";
 $password = "root";
@@ -13,54 +13,24 @@ if ($conn->connect_error) {
 	}
 //mysqli_close($conn);
 mysqli_select_db($conn,'animal_shelter');
-$inputuser = mysql_real_escape_string($_POST['user']);
-$inputpass = mysql_real_escapte_string($_POST['pass']);
 
-$query="SELECT * from `profile` WHERE `Username` ='$inputuser' AND `password	`='$inputpass'";
+$inputuser = mysqli_real_escape_string($conn, $_POST["user"]);
+$inputpass = mysqli_real_escape_string($conn, $_POST["pass"]);
 
+$sql = "SELECT profile_id FROM profile WHERE username = '$inputuser' and password = '$inputpass'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$active = $row['active'];
 
-$result=mysqli_query($conn,$query);
-
-if(mysql_num_rows($result)==1){
-	header('Location: database.php');
-	die();
+$count = mysqli_num_rows($result);
+if($count == 1)
+{
+	header("location: database.php");
 }
-else {
-	header('Location: fail.php');
-}
-
-/*$row=mysqli_fetch_array($result);
-$rowpass=mysqli_fetch_array($resultpass);
-
-$serveruser=$row["username"];
-$serverpass=$row["password"];
-
-if($serveruser && $serverpass){
-	if(!$result){
-		die("Username or password is invalid");
-	}
-	
-	echo "<br><center> Database output</b></center><br><br>";
-	echo $inputpass;
-	echo $serverpass;
-}
-if($inputpass==$serverpass){
-	header('Location: database.php');
-}
-else {
-	//header ('Location: fail.php');
-}
-//header('Location:fail.php');
-
-mysqli_close($conn);
-/*if( $user== "sergiogee" && $pass=="dallas"){
-	echo "welcome"."\n".$user;
-	header("Location:database.php");
+else
+{
+	header("location: fail.php");
 }
 
-else {
-	echo "incorrect login";
-	header("Location:fail.php");
-}*/
 mysqli_close($conn);
 ?>
